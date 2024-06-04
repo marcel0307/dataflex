aa909bccce5c751384bae585da18e10c74e7b70e
marcel0307 <63732059+marcel0307@users.noreply.github.com>
2024-06-04 14:34:04 +0400

    small refactoring

--- pandas.py
+++ pandas.py
@@ -1,51 +1,45 @@
+# pandas.py
+
 import polars as pl
 
-class DataFrame:
-    """
-    DataFrame class that has the exact same user interface as pandas, but executing Polars transformations under the hood.
-    """
-    def __init__(self, data: dict) -> None:
-        self._df = pl.DataFrame(data)
+class Base:
+    def __init__(self, data):
+        self.data = data
 
     def head(self, n=5):
-        return self._df.head(n)
-
-    def merge(self, df_right, how: str, on: str):
-        return DataFrame(self._df.join(df_right._df, how=how, on=on))
+        return self.data.head(n)
 
     @property
     def loc(self):
-        return DataFrameLocator(self._df)
+        return Locator(self.data)
 
-class DataFrameLocator:
-    def __init__(self, df):
-        self.df = df
+class Locator:
+    def __init__(self, data):
+        self.data = data
 
     def __getitem__(self, key):
-        # Case 1: Single label (scalar)
-        if not isinstance(key, tuple):
-            key = (key, slice(None))
-
-        # Case 2: Tuple (row_key, col_key)
-        row_key, col_key = key
-
-        # Handle row selection
-        # TODO: check if slice start/stop need to be inclusive or exclusive
-        if isinstance(row_key, slice):
-            df = self.df.slice(row_key.start, row_key.stop - row_key.start)
-        elif isinstance(row_key, list):
-            df = self.df.filter(pl.col('index').is_in(row_key))
-        elif isinstance(row_key, pl.Series):
-            df = self.df.filter(row_key)
-        elif callable(row_key):
-            df = self.df.filter(row_key(self.df))
+        if isinstance(key, slice):
+            data = self.data.slice(key.start, key.stop - key.start)
+        elif isinstance(key, list):
+            data = self.data.filter(pl.col('index').is_in(key))
+        elif isinstance(key, Series):
+            data = self.data.filter(key.data)
+        elif callable(key):
+            data = self.data.filter(key(self.data))
         else:
-            df = self.df.filter(pl.col('index') == row_key)
+            data = self.data.filter(pl.col('index') == key)
 
-        # Handle column selection
-        if isinstance(col_key, slice):
-            col_key = self.df.columns[col_key]
-        elif callable(col_key):
-            col_key = [col for col in self.df.columns if col_key(self.df[col])]
+        return data
 
-        return df.select(col_key)
+class DataFrame(Base):
+    def __init__(self, data: dict) -> None:
+        super().__init__(pl.DataFrame(data))
+
+    def merge(self, df_right, how: str, on: str):
+        return DataFrame(self.data.join(df_right.data, how=how, on=on))
+
+class Series(Base):
+    def __init__(self, data: list) -> None:
+        super().__init__(pl.Series(data))
+
+    
--- test.py
+++ test.py
@@ -1,10 +1,10 @@
-import polars as pl
 import pandas as pd
 
 data = {
     'colA': [1, 2, 4, 7, 10],
     'colB': [10, 20, 30, 40, 50],
-    'index': [4, 5, 6, 7, 8]
+    'index': [4, 5, 6, 7, 8],
+    'test': ["abc", "joij", "def", "iji", "ioj"]
 }
 df = pd.DataFrame(data)
 
@@ -19,12 +19,11 @@ print(df.loc[[4, 6], ['colA', 'colB']])
 # Slice
 print(df.loc[4:7])
 
-# Tuple (row_key, col_key)
-print(df.loc[(4, 6), 'colA'])
-
 # Boolean Series
-bool_series = pl.Series([True, False, True, False, True])
+bool_series = pd.Series([True, False, True, False, True])
 print(df.loc[bool_series])
 
 # Callable function
 print(df.loc[lambda df: df['colA'] > 2])
+
+df[(df["test"].str.lower() == "abc") | (df["test"].str.lower() == "def")]
