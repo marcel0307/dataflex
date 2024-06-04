476d50f01bf0434947db3b817baa733e2e8ff2ff
marcel0307 <63732059+marcel0307@users.noreply.github.com>
2024-06-04 13:38:50 +0200

    Create initial code structure (#1)
    * test commit
    
    * implement Locator
    
    * small refactoring
    
    * implement StringMethods
    
    * automatic commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+data = {
+    'colA': [1, 2, 4, 7, 10],
+    'colB': [10, 20, 30, 40, 50],
+    'index': [4, 5, 6, 7, 8],
+    'test': ["abc", "joij", "def", "iji", "ioj"]
+}
+df = pd.DataFrame(data)
+
+print(df.head())
+
+# Single label
+print(df.loc[4])
+
+# List of labels
+print(df.loc[[4, 6], ['colA', 'colB']])
+
+# Slice
+print(df.loc[4:7])
+
+# Boolean Series
+bool_series = pd.Series([True, False, True, False, True])
+print(df.loc[bool_series])
+
+# Callable function
+print(df.loc[lambda df: df['colA'] > 2])
+
+print(df[(df["test"].str.lower() == "abc") | (df["test"].str.lower() == "def")].head())
